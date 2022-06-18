015f7c9758510bb30c295f05c28fe05063800018
Singee <git@singee.me>
2022-06-18 19:19:53 +0800

    Stage 1/4

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,1 +1,24 @@
-# Write your code here
+import sys
+from lxml import etree
+from nltk.tokenize import word_tokenize
+from collections import Counter
+
+if __name__ == '__main__':
+    stdin = open("news.xml", "rb").read()
+
+    xml = etree.fromstring(stdin)
+    corpus = xml[0]
+    for news in corpus:
+        head = news[0].text
+        text = news[1].text
+
+        tokens = word_tokenize(text.lower())
+        counter = Counter(tokens)
+
+        top5 = counter.most_common(10)
+        top5.sort(key=lambda x: x[0], reverse=True)
+        top5.sort(key=lambda x: x[1], reverse=True)
+        top5 = top5[:5]
+
+        print(head, end=':\n')
+        print(" ".join([x[0] for x in top5]))
