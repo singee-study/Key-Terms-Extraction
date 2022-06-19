3a2687eac739333e1667d726f4c32f7af24f63ad
Singee <git@singee.me>
2022-06-19 14:54:46 +0800

    Stage 2

--- Key Terms Extraction/task/key_terms.py
+++ Key Terms Extraction/task/key_terms.py
@@ -1,10 +1,15 @@
-import sys
 from lxml import etree
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
 from collections import Counter
 
 if __name__ == '__main__':
     stdin = open("news.xml", "rb").read()
+
+    lemmatizer = WordNetLemmatizer()
+    ignores = set(list(stopwords.words('english')) + list(string.punctuation))
 
     xml = etree.fromstring(stdin)
     corpus = xml[0]
@@ -13,6 +18,9 @@ if __name__ == '__main__':
         text = news[1].text
 
         tokens = word_tokenize(text.lower())
+        tokens = [lemmatizer.lemmatize(x) for x in tokens]
+        tokens = [x for x in tokens if x and x not in ignores]
+
         counter = Counter(tokens)
 
         top5 = counter.most_common(10)
